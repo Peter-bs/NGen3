85037427efd2266fd963f37874590a95c4aaf26d
Peter Bou Saada <peterbousaada@gmail.com>
2019-03-21 16:11:40 -0400

    Improved performance for all renderers

--- GameEngine-core/main.cpp
+++ GameEngine-core/main.cpp
@@ -1,3 +1,5 @@
+#include <time.h>
+
 #include "src\graphics\window.h"
 #include "src\graphics\shader.h"
 #include "src\maths\maths.h"
@@ -13,6 +15,7 @@
 
 #include "src/graphics/static_sprite.h"
 #include "src/graphics/sprite.h"
+#include "src\utils\timer.h"
 
 #define BATCH_RENDERER 1
 
@@ -31,10 +34,33 @@ int main()
 	shader.enable();
 	shader.setUniformMat4("pr_matrix", ortho);
 
+	std::vector<Renderable2D*> sprites;
+
+	srand(time(NULL));
+
+	for (float y = 0; y < 9.0f; y+= 0.05)
+	{
+		for (float x = 0; x < 16.0f; x+= 0.05)
+		{
+			sprites.push_back(new 
+#if BATCH_RENDERER
+			Sprite
+#else
+			StaticSprite
+#endif
+			(x, y, 0.04f, 0.04f, maths::vec4(rand() % 1000 / 1000.0f, 0, 1, 1)
+#if !BATCH_RENDERER
+				, shader
+#endif
+				));
+		}
+	}
+
 #if BATCH_RENDERER
 	Sprite sprite(0,0,8,9, vec4(0.8,0.2,0.2,1.0));
 	Sprite sprite2(8,0,8,9, vec4(0.2, 0.2, 0.8,1.0));
 	BatchRenderer2D renderer;
+
 #else
 	StaticSprite sprite(0, 0, 8, 9, vec4(0.8, 0.2, 0.2, 1.0), shader);
 	StaticSprite sprite2(8, 0, 8, 9, vec4(0.2, 0.2, 0.8, 1.0), shader);
@@ -44,25 +70,36 @@ int main()
 	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
 	shader.setUniform4f("col", vec4(0.1f, 0.3f, 0.7f, 1.0f));
 
-
+	Timer time;
+	float timer = 0;
+	unsigned int frames = 0;
+	double x, y;
+	int width, height;
 	while (!window.closed())
 	{
 		window.clear();
-		double x, y;
-		int width, height;
 		window.getMousePosition(x, y);
 		window.getWindowSize(width, height);
 		shader.setUniform2f("light_pos", vec2( (float)(x * 16.0f / width), (float)(9.0f - y * 9.0f / height)));
 #if BATCH_RENDERER
 		renderer.begin();
 #endif
-		renderer.submit(&sprite);
-		renderer.submit(&sprite2);
+		for (int i = 0; i < sprites.size(); i++)
+		{
+			renderer.submit(sprites[i]);
+		}
 #if BATCH_RENDERER
 		renderer.end();
 #endif
 		renderer.flush();
 		window.update();
+		frames++;
+		if (time.elapsed() - timer > 1.0f)
+		{
+			timer += 1.0f;
+			printf("%d fps\n", frames);
+			frames = 0;
+		}
 	}
 
 	return 0;
